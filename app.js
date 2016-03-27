cbba9d8a72d722d31d6db0e732f9c292b6365d5e
cairnstan <cairnsolsen@gmail.com>
2016-03-27 14:10:29 -0500

    Add 5 new objects to array, create functions to calculate percent covered and amount covered.

--- app.js
+++ app.js
@@ -37,7 +37,47 @@ function claim(name, type, cost){
 	this.visitType = type;
 	this.visitCost = cost;
 }
+var claim6 = new claim('Bob Roberts', 'Primary Care', 220);
+var claim7 = new claim('Little Mermaid', 'Emergency', 1800);
+var claim8 = new claim('Bob Dylan', 'Specialist', 3000);
+var claim9 = new claim('Patti Smith', 'Optical', 50);
+var claim10 = new claim('Ms Marvel', 'Emergency', 4200);
 
+initialList.push(claim6, claim7, claim8, claim9, claim10);
+console.log(initialList);
+
+for (var it = 0; it < initialList.length; it++) {
+console.log((amountPaid(initialList[it]) * 100) + '%');
+console.log(covered(initialList[it]))
+}
+function amountPaid(claim){
 //function to determine percent covered
+var visitType = claim.visitType;
+switch(visitType){
+	case 'Optical':
+	percentCovered = 0.0;
+	break;
+	case 'Specialist':
+	percentCovered = 0.1;
+	break;
+	case 'Emergency':
+	percentCovered = 1.0;
+	break;
+	case 'Primary Care':
+	percentCovered = 0.5;
+	break;
+	default:
+	percentCovered = 0.0;
+}
+
+return percentCovered;
+}
+
 
 //function to determine amount covered
+function covered(claim){
+var amountCovered = Math.round(percentCovered * initialList[it].visitCost);
+
+
+console.log('Paid out $' + amountCovered + ' for ' + initialList[it].patientName);
+}
